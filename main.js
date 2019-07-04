75ac90549466249310e71bd534378a9f0dc6ca23
Michal <michal.postek1@wp.pl>
2019-07-04 20:18:18 +0200

    Feature: added moreDetail Site, more css,

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-let searchMovie = document.getElementById('searchText');
+const searchMovie = document.getElementById('searchText');
 
 document.getElementById('searchForm').addEventListener('submit', (e) => {
     e.preventDefault();
     let searchText = searchMovie.value;
 
     getMovies(searchText);
-
     document.getElementById('searchForm').reset();
 })
 
@@ -21,21 +20,63 @@ function getMovies(searchText) {
             let output = '';
             movies.forEach((movie, index) => {
                 output += `
-            <div class="movie-box">
-            <img src='${movie.Poster}'>
-            <div class="movie-box-info">
-                <div>
-                    <h4>Premier Date: ${movie.Year}</h4>
-                    <h3>${movie.Title}</h3>
+                <div class="movie-box">
+                <img src='${movie.Poster}'>
+                <div class="movie-box-info">
+                    <div>
+                        <h3>${movie.Title}</h3>
+                        <h4>Premier Date: ${movie.Year}</h4> 
+                    </div>
+                    <a onclick="movieSelected('${movie.imdbID}')" href="./movie.html">Learn more</a>
                 </div>
-                <a onclick="movieSelected('${movie.imdbID}')" href="https://www.imdb.com/title/${movie.imdbID}" target ="_blank">More info of movie on IMDb</a>
-            </div>
-            </div>
-            `
+                </div>
+                `
             });
             document.querySelector('.movies').innerHTML = output;
         }
     }
     xhr.send()
 
+}
+
+function movieSelected(id) {
+    sessionStorage.setItem('id', id)
+}
+
+function getMovie(id) {
+
+    let movieId = sessionStorage.getItem('id');
+
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://www.omdbapi.com/?apikey=f31a4db7&i=${movieId}`, true);
+
+    xhr.onload = function () {
+        if (this.status == 200) {
+            let movieData = JSON.parse(this.responseText);
+            let movie = movieData;
+            let output = '';
+            output += `
+                <div class="movie-box-single">
+                <img src='${movie.Poster}'>
+                <div class= "list">
+                    <ul>
+                        <li><strong>Director:</strong> <span>${movie.Director}</span></li>
+                        <li><strong>IMDb Rating:</strong> <span>${movie.imdbRating}/10 (${movie.imdbVotes} votes)</span></li>
+                        <li><strong>Released:</strong> <span>${movie.Released}</span></li>
+                        <li><strong>Length:</strong> <span>${movie.Runtime}</span></li>
+                        <li><strong>Genre:</strong> <span>${movie.Genre}</span></li>
+                    </ul>
+                </div>
+                </div>
+                <div class="btn">
+                    <a href="./index.html">Go back</a>
+                    <a href="https://www.imdb.com/title/${movie.imdbID}">Check IMDb for more info</a>
+                </div>
+                `;
+
+            document.querySelector('.movies').innerHTML = output;
+        }
+    }
+    xhr.send()
 }
